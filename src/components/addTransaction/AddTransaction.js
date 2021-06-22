5e229ddfe3dfe0858ca85781c03a6c65ccf092b6
trymycode <imitasingha1994@gmail.com>
2021-06-22 17:14:44 +0530

    add expense type in the transaction form

--- src/components/addTransaction/AddTransaction.js
+++ src/components/addTransaction/AddTransaction.js
@@ -5,13 +5,21 @@ function AddTransaction() {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
   const { addTransaction, transactions } = useContext(GlobalContext);
+  const [typeOfTransection, changedTypeOfTransection] = useState(null);
   const onSubmit = (e) => {
-    e.preventDefault();
-    let id = transactions.length + 1;
-    let transaction = { id, text, amount: +amount };
-    addTransaction(transaction);
-    setText("");
-    setAmount(0);
+    if (typeOfTransection != null && amount) {
+      e.preventDefault();
+      let id = transactions.length + 1;
+      let amountInNumber = amount && +amount;
+      let amountWithType =
+        typeOfTransection === "Incoming" ? amountInNumber : -amountInNumber;
+      let transaction = { id, text, amount: amountWithType };
+      addTransaction(transaction);
+      setText("");
+      setAmount(0);
+    } else {
+      alert("Please select type of transaction!");
+    }
   };
 
   return (
@@ -19,7 +27,7 @@ function AddTransaction() {
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control mb-2 p-2">
-          <label htmlFor="text">Describe Expense</label>
+          <label htmlFor="text">Describe Transaction</label>
           <input
             type="text"
             value={text}
@@ -28,9 +36,30 @@ function AddTransaction() {
           />
         </div>
         <div className="form-control mb-2 p-2">
-          <label htmlFor="amount">
-            Amount (negative - expense, positive - income)
-          </label>
+          <label htmlFor="amount"> Enter Amount</label>
+          <p className="transaction-type mt-1">
+            {" "}
+            <label className="pr-1">
+              <input
+                type="radio"
+                value="Incoming"
+                className="pr-1 mr-2"
+                checked={typeOfTransection === "Incoming"}
+                onChange={() => changedTypeOfTransection("Incoming")}
+              />
+              Incoming
+            </label>
+            <label className="pr-1">
+              <input
+                type="radio"
+                value="Outgoing"
+                className="pr-1 mr-2"
+                checked={typeOfTransection === "Outgoing"}
+                onChange={() => changedTypeOfTransection("Outgoing")}
+              />
+              Outgoing
+            </label>
+          </p>
           <input
             type="number"
             value={amount}
